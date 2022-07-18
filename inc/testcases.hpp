4beccdd7291f485020edddcf513da9948289b55a
Anish-08 <96428233+Anish-08@users.noreply.github.com>
2022-07-18 18:25:50 +0530

    Test cases

--- inc/testcases.hpp
+++ inc/testcases.hpp
@@ -4,19 +4,16 @@
 std::vector<Physicc::RigidBody> testcases(int x)
 {
     std::vector<Physicc::RigidBody> mylist;
-
-    Physicc::RigidBody a1,a2,a3,a4; 
-	glm::vec3 v1{0,0,0}, v10{8,4,0} , v2{3,8,0}, v20{8,10,0} , v3{5,3,0}, v30{7,9,0},v4{10,2,0},v40{14,4,0};
-	
-	a1.SetThisVolume(v1,v10);
-	a2.SetThisVolume(v2,v20);
-	a3.SetThisVolume(v3,v30);
-	a4.SetThisVolume(v4,v40);
-
-	mylist.push_back(a1);
-	mylist.push_back(a2);
-	mylist.push_back(a3);
-	mylist.push_back(a4);
+	glm::vec3 lb[x+1] , ub[x+1];
+	Physicc::RigidBody a[x+1];
+	for(int i = 0; i < x; i++){
+        lb[i].x = -100 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200)));
+        lb[i].y = -100 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200)));
+        ub[i].x = -100 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200)));
+        ub[i].y = -100 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200)));
+		a[i].SetThisVolume(lb[i],ub[i]);
+		mylist.push_back(a[i]);
+	}
 
     return mylist;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include "bvh.hpp"
 #include "broadphase.hpp"
 #include "testcases.hpp"
+#include<chrono>
+
+using namespace std;
+using namespace std::chrono;
 
 int main()
 { 
@@ -23,22 +27,37 @@ int main()
 	mylist.push_back(a3);
 	mylist.push_back(a4);
 	*/
-	int x;
-	x = 500;
-	std::vector<Physicc::RigidBody> mylist = testcases(x);
+	std::vector<Physicc::RigidBody> mylist = testcases(500);
+	std::vector<Physicc::Broadphase::PotentialContact> variable, variable1;
 	Physicc::BVH mybvh{mylist};
-	mybvh.buildTree();
-	
 
-	std::vector<Physicc::Broadphase::PotentialContact> variable;
+
+	//auto start = high_resolution_clock::now();
+
+	mybvh.buildTree();
 	variable = Physicc::Broadphase::getPotentialContacts(mybvh);
+
+	//auto stop = high_resolution_clock::now();
+    //auto duration = duration_cast<microseconds>(stop - start);
+
+
 	int n = variable.size();
 	std::cout<<"Number of collisions : " <<n<<'\n';
-	for(int i = 0 ; i< n ; i++)
+
+	/*auto start = high_resolution_clock::now();
+
+	variable1 = brute();
+
+	auto stop = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop - start);
+	*/
+	cout<<"Yeah it worked"<<endl;
+	//cout << "Time taken by bvh: "<< duration.count() << " microseconds" << endl;
+	//cout << "Time taken by brute force: "<< duration1.count() << " microseconds" << endl;
+	return 0;
+
+	/*for(int i = 0 ; i< n ; i++)
 	{
 		variable[i].printcontact();
-	}
-	std::cout<<"Yeah it worked";
-
-	return 0;
+	}*/
 }
