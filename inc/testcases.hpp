b63f20acd40322faa0760ac841432525510ce526
Anish-08 <96428233+Anish-08@users.noreply.github.com>
2022-08-01 17:27:22 +0530

    Minor Changes

--- inc/testcases.hpp
+++ inc/testcases.hpp
@@ -10,8 +10,8 @@ std::vector<Physicc::RigidBody> testcases(int x)
 	for(int i = 0; i < x; i++){
         lb[i].x = -100 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200)));
         lb[i].y = -100 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200)));
-        ub[i].x = lb[i].x + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(100)));
-        ub[i].y = lb[i].y + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(100)));
+        ub[i].x = lb[i].x + 0.2;
+        ub[i].y = lb[i].y + 0.2;
 		a[i].SetThisVolume(lb[i],ub[i]);
 		mylist.push_back(a[i]);
 	}
--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ using namespace std::chrono;
 int main()
 { 
 
-	int x = 50000;
+	int x = 30000;
 	std::vector<Physicc::RigidBody> mylist ;
 	mylist = testcases(x);
 	std::vector<Physicc::Broadphase::PotentialContact> variable;
@@ -22,40 +22,32 @@ int main()
 
 	auto start = high_resolution_clock::now();
 	mybvh.buildTree();
-	//variable = Physicc::Broadphase::getPotentialContacts(mybvh);
+	variable = Physicc::Broadphase::getPotentialContacts(mybvh);
 	auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
-
-
-	auto stort = high_resolution_clock::now();
-
-	variable = Physicc::Broadphase::getPotentialContacts(mybvh);
-
-	auto stap = high_resolution_clock::now();
-    auto duration3 = duration_cast<microseconds>(stap - stort);
 	
 
 	int n = variable.size();
-	//cout<<"Number of collisions : " <<n<<'\n';
+	cout<<"Number of collisions in bvh : " <<n<<'\n';
 
 	auto what = high_resolution_clock::now();
 
 	int ans = 0;
 	for(int i = 0 ; i < x ; i++){
-		for(int j = i+1; j<x ; j++){
-			if(mylist[i].getAABB().overlapsWith(mylist[j].getAABB())) ans++;
+		for(int j = 0; j<x ; j++){
+			if((j!=i) && mylist[i].getAABB().overlapsWith(mylist[j].getAABB())) ans++;
 		}
 	}
-
+	ans  = ans / 2;
 	auto which = high_resolution_clock::now();
     auto duration1 = duration_cast<microseconds>(which - what);
-	//cout<<"Number of collisions 1 : " <<ans<<'\n';
+	cout<<"Number of collisions in brute force : " <<ans<<'\n';
 
 	cout << "Time taken by bvh: "<< duration.count() << " microseconds" << endl;
-	cout << "Time taken by broad: "<< duration3.count() << " microseconds" << endl;
+	//cout << "Time taken by broad: "<< duration3.count() << " microseconds" << endl;
 	cout << "Time taken by brute force: "<< duration1.count() << " microseconds" << endl;
 	
-	if(n == ans){cout<<"Yeah it worked"<<endl;}
+	if(n == ans){cout<<"The answer matched"<<endl;}
 	return 0;
 
 	/*for(int i = 0 ; i< n ; i++)
